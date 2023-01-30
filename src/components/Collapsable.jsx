e2d3a212c1df24e843b01bbc2b8feaf7b3ffc95a
Alan Parra <dev.alanparra@gmail.com>
2023-01-30 15:49:51 -0600

    new sizes

--- src/components/Collapsable.jsx
+++ src/components/Collapsable.jsx
@@ -8,7 +8,7 @@ import { colores } from '../scss/colores';
 
 const Titulo = styled.h3`
     color: ${colores.principal};
-    font-size: 22px;
+    font-size: 21px;
     cursor: pointer;
     border-bottom: 1px solid gainsboro;
     margin-bottom: 10px;
